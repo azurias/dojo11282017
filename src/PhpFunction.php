09c0ebe1e3a6cbefc058b6ae9583988aeb7aa691
Matthieu Caradet <Matthieu.caradet@gmail.com>
2017-11-28 10:23:07 +0100

    base dossier test/public

---
+++ src/PhpFunction.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder15
+ * Date: 28/11/17
+ * Time: 09:57
+ */
+
+namespace Wcs;
+
+
+class PhpFunction
+{
+
+
+}
---
+++ tests/PhpFunctionTests.php
@@ -0,0 +1,10 @@
+<?php
+
+use PHPUnit\Framework;
+
+/**
+ * Created by PhpStorm.
+ * User: wilder15
+ * Date: 28/11/17
+ * Time: 09:58
+ */
